fc3d8e7fe2768d4b16433235004b87f59b504645
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-06-27 12:51:29 -0300

    Create create_tables.sql
    Adicionado arquivo SQL de criação de tabelas

---
+++ arquivos_sql/create_tables.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE `game_of_thrones`;
+
+USE game_of_thrones;
+
+-- criar tabelas separadas, uma por uma.
+
+CREATE TABLE `casas` (
+    id_casa INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255),
+    regiao VARCHAR(255)
+);
+
+CREATE TABLE `personagem` (
+    id_personagem INT AUTO_INCREMENT PRIMARY KEY,
+    nome_personagem VARCHAR(255),
+    nome_autor VARCHAR(255),
+    id_casa INT,
+    FOREIGN KEY (id_casa)
+        REFERENCES casas (id_casa),
+    qnt_episodios INT,
+    primeira_aparicao INT,
+    ultima_aparicao INT,
+    sexo VARCHAR(1)
+);
+
+CREATE TABLE `episodios` (
+    episodio_id INT AUTO_INCREMENT PRIMARY KEY,
+    temporada INT,
+    episodio INT,
+    titulo VARCHAR(255),
+    data_lancamento DATE,
+    nota INT,
+    qnt_votos INT,
+    escritor VARCHAR(255),
+    estrela_1 INT,
+    FOREIGN KEY (estrela_1)
+        REFERENCES personagem (id_personagem),
+    estrela_2 INT,
+    FOREIGN KEY (estrela_2)
+        REFERENCES personagem (id_personagem),
+    estrela_3 INT,
+    FOREIGN KEY (estrela_3)
+        REFERENCES personagem (id_personagem),
+    review_usuario INT,
+    review_critica INT,
+    duracao INT,
+    diretor VARCHAR(255),
+    orcamento INT
+);
